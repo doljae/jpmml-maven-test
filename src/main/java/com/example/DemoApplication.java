7a4578334c9876578f734a6b38e9a88e2df25da6
doljae <seok9211@naver.com>
2022-05-19 18:57:06 +0900

    check error reproducing

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jpmml.evaluator.LoadingModelEvaluatorBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xml.sax.SAXException;
+
+import jakarta.xml.bind.JAXBException;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        File file = null;
+        try {
+            new LoadingModelEvaluatorBuilder().load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException | JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
 }

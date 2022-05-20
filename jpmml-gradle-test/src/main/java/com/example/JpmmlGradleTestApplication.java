ccf201141f67fc344831f4f0c61f9ceb1f738be6
doljae <seok9211@naver.com>
2022-05-20 09:04:00 +0900

    add jpmml springboot module using gradle script

---
+++ jpmml-gradle-test/src/main/java/com/example/JpmmlGradleTestApplication.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jpmml.evaluator.LoadingModelEvaluatorBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xml.sax.SAXException;
+
+@SpringBootApplication
+public class JpmmlGradleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpmmlGradleTestApplication.class, args);
+
+        File file = null;
+        try {
+            new LoadingModelEvaluatorBuilder().load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ jpmml-gradle-test/src/main/java/com/example/domain/Employee.java
@@ -0,0 +1,20 @@
+package com.example.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+}
